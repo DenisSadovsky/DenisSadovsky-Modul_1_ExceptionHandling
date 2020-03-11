d8a03b4ff8bb578daa8ca23a62ae4cc105d2f296
Dzianis Sadouski <Dzianis_Sadouski@epam.com>
2020-03-11 17:18:50 +0300

    Add project files.

---
+++ Modul_1_ExH_2/ConverterCustom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Modul_1_ExH_2
+{
+    public static class ConverterCustom
+    {
+        public static void ConvertStrToInt(string str)
+        {
+            try
+            {
+                int number = Convert.ToInt32(str.Substring(0, 1));
+                Console.WriteLine($"First character number - {number}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Input string is not a sequence of digits.");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("The number cannot fit in an Int32.");
+            }
+        }
+    }
+}
---
+++ Modul_1_Exception_Handling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Modul_1_ExH_2;
+
+namespace Modul_1_Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("\nEnter a string to analyze");
+                string StrIn = Console.ReadLine();
+                try
+                {
+                    CheckFirstSymbol(StrIn);            
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine($"An exception has occurred, {e.Message}");
+                    throw new ArgumentOutOfRangeException("index parameter is out of range.", e);
+                }
+
+                Console.WriteLine("\nCheck the first character for a number\n");
+                ConverterCustom.ConvertStrToInt(StrIn);
+
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+        public static void CheckFirstSymbol(string str)
+        {
+            if (str.Substring(0, 1) != "")
+            {
+                Console.WriteLine($"{str.Substring(0, 1)}");
+            }
+
+        }
+    }
+}
